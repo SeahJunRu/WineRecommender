3a54f42f54f66c687d22289d9324ad649d3df753
Junru1231 <junru1231@gmail.com>
2018-09-25 01:25:44 +0800

    initial web app created

--- wine_recommender/reviews/urls.py
+++ wine_recommender/reviews/urls.py
@@ -13,12 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from reviews import views
+
+
+
+from django.urls import include, re_path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('reviews/', include(('reviews.urls', 'reviews'), namespace='reviews')),
-	
+    # ex: /
+    re_path(r'^$', views.review_list, name='review_list'),
+    # ex: /review/5/
+    re_path(r'^review/(?P<review_id>[0-9]+)/$', views.review_detail, name='review_detail'),
+    # ex: /wine/
+    re_path(r'^wine$', views.wine_list, name='wine_list'),
+    # ex: /wine/5/
+    re_path(r'^wine/(?P<wine_id>[0-9]+)/$', views.wine_detail, name='wine_detail'),
+	re_path(r'^wine/(?P<wine_id>[0-9]+)/add_review/$', views.add_review, name='add_review'),
 ]
